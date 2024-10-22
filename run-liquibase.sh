2d665917ef739778f644783a040575c0a55c4f32
HITimran <HitImran@gmail.com>
2024-10-22 12:13:30 -0700

    +

--- run-liquibase.sh
+++ run-liquibase.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting run-liquibase.sh script"
 
 # Wait for PostgreSQL to start
-until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+until pg_isready -h postgres -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
   echo "Waiting for PostgreSQL to start..."
   sleep 2
 done
@@ -14,7 +14,7 @@ echo "PostgreSQL is ready. Running Liquibase..."
 # Run Liquibase
 $LIQUIBASE_HOME/liquibase \
   --changelog-file=/liquibase/changelog.xml \
-  --url="jdbc:postgresql://localhost:5432/$POSTGRES_DB" \
+  --url="jdbc:postgresql://postgres:5432/$POSTGRES_DB" \
   --username="$POSTGRES_USER" \
   --password="$POSTGRES_PASSWORD" \
   --logLevel=DEBUG \
