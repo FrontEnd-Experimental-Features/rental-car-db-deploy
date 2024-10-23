26b855899721e1b47cb4950b284889ba232ba0be
HITimran <HitImran@gmail.com>
2024-10-22 21:37:03 -0700

    hamburger menu

--- liquibase/v2/hamburger-menu/insertion.sql
+++ liquibase/v2/hamburger-menu/insertion.sql
@@ -1,3 +1,30 @@
-INSERT INTO public."OptionsHamburgerMenu"(
-	"Id", "IsActive", "Name")
-	VALUES (?, ?, ?);
+-- Insert main menu items
+INSERT INTO public."OptionsHamburgerMenu" ("Name", "IsActive")
+VALUES 
+('Reservations', true),
+('Vehicles', true),
+('Locations', true),
+('Deals', true);
+
+-- Insert submenu items
+INSERT INTO public."OptionsHamburgerSubMenu" ("SubMenu", "SubMenuOptions", "IsActive")
+VALUES 
+-- Reservations submenu
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Reservations'), 'Make a Reservation', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Reservations'), 'View/Modify/Cancel', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Reservations'), 'Get a Receipt', true),
+
+-- Vehicles submenu
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Vehicles'), 'Car Guide', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Vehicles'), 'Luxury Cars', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Vehicles'), 'Special Offers', true),
+
+-- Locations submenu
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Locations'), 'Find a Location', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Locations'), 'Airport Rentals', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Locations'), 'International Rentals', true),
+
+-- Deals submenu
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Deals'), 'Current Promotions', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Deals'), 'Last Minute Deals', true),
+((SELECT "Id" FROM public."OptionsHamburgerMenu" WHERE "Name" = 'Deals'), 'Partner Offers', true);
